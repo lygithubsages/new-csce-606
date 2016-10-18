87c7e53d25811b6889b9965c1eeb5ec6c7482301
Y L <lysages@163.com>
2016-10-18 14:33:26 -0500

    part 4 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,19 +11,31 @@ class MoviesController < ApplicationController
   end
 
   def index
+    
+    redirect=false
+    @sort = params[:sort] ? params[:sort] : session[:sort]
+    @ratings = params[:ratings] ? params[:ratings] : session[:ratings]
     @all_ratings = Movie.pluck(:rating).uniq
 
-    if params[:sort]=='title'
-      @movies = Movie.order('title ASC')
-      @title_hilite = 'hilite'
-    elsif params[:sort]=='release'
-      @movies = Movie.order('release_date ASC')
-      @release_hilite = 'hilite'
-    else
-      params[:ratings] ? @movies=Movie.where(rating: params[:ratings].keys) : @movies = Movie.all
+    if (params[:sort] != @sort) || (params[:ratings] != @ratings)
+       redirect=true
+    
+    end 
 
-  end 
+    if @ratings.nil?
+      @ratings = {}
+      @all_ratings.each{|i| @ratings[i] = 1}
+    end
+
+  redirect_to movies_path(:sort=>@sort, :ratings=>@ratings) if redirect
+ 
+   @movies = Movie.where(:rating=> @ratings.keys).order(@sort)
+  
+    session[:sort] = @sort
+    session[:ratings] = @ratings
+
   end
+
 
   def new
     # default: render 'new' template
